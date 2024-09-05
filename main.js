c66c9ff65f533fa25a0502a7d1bcf47d284610a8
yilbert luna <yilbert.luna.e@gmail.com>
2024-09-04 23:16:15 -0400

    feat(database): establish connection to the database

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-import app from "./src/server/server.js";
 import "dotenv/config"
+
+import app from "./src/server/server.js";
+import { connectDb } from "./src/dataBase/connectionDb.js";
 
 const PORT = process.env.PORT;
 
-function startServer() {
+async function startServer() {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT} 🟢`);
     });
+
+    await connectDb()
 }
 
 startServer();
